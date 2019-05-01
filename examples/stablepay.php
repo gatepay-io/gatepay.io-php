099f8dfafedbd097c8b6bd5029d8ae2cab8f4768
terry <iterrypeng@gmail.com>
2019-05-01 14:24:34 +0800

    Update stablepay.php

--- examples/stablepay.php
+++ examples/stablepay.php
@@ -1,1 +1,22 @@
 <?php
+require_once '../src/init.php';
+use gatepayio\Api;
+/*
+  Gatepay SDK 任意金额支付演示 
+*/
+$appkey = 'your appkey';
+$appsecret = 'your appsecret';
+$price = 0.01;
+$type = 'wechat';
+$custom = 'your custom info';
+$out_order_id = uniqid();//your order system id
+$product_id = 'your product_id in gatepay system';
+$response = $api->auth($appkey,$appsecret)->sign([
+	'price'=>$price,
+	'type'=>$type,
+	'out_order_id'=>$out_order_id,
+	'custom'=>$custom,
+  'product_id'=>$product_id,
+])->route('stablepay','create')->request();
+var_dump($response);
+?>
